3dacdc6276eb84082c3b5390208658a4f66b4687
ACNers <acnamritachennai@gmail.com>
2024-08-30 18:23:13 +0530

    Update fetch-config.js

--- api/fetch-config.js
+++ api/fetch-config.js
@@ -5,15 +5,16 @@ export default async function handler(req, res) {
         const firebaseConfig = {
             apiKey: await kv.get("FIREBASE_API_KEY"),
             authDomain: await kv.get("FIREBASE_AUTH_DOMAIN"),
-            projectId: await kv.get("FIREBASE_PROJECT_ID"),
-            storageBucket: await kv.get("FIREBASE_STORAGE_BUCKET"),
-            messagingSenderId: await kv.get("FIREBASE_MESSAGING_SENDER_ID"),
+            measurementId: await kv.get("FIREBASE_MEASUREMENT_ID"),
             appId: await kv.get("FIREBASE_APP_ID"),
-            measurementId: await kv.get("FIREBASE_MEASUREMENT_ID")
+            messagingSenderId: await kv.get("FIREBASE_MESSAGING_SENDER_ID"),
+            storageBucket: await kv.get("FIREBASE_STORAGE_BUCKET"),
+            projectId: await kv.get("FIREBASE_PROJECT_ID")
         };
+
         res.status(200).json(firebaseConfig);
     } catch (error) {
+        console.error('Error fetching Firebase config:', error);
         res.status(500).json({ error: 'Failed to fetch Firebase configuration' });
     }
 }
-
